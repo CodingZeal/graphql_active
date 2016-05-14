8ef7d03301e0e3cec5173908a676d4c68502628b
Julian Rogmans <rogmansj@gmail.com>
2016-05-13 17:31:07 -0700

    Remove useless tests from base gem install

--- spec/easy_graph_spec.rb
+++ spec/easy_graph_spec.rb
@@ -1,11 +1,4 @@
 require 'spec_helper'
 
 describe EasyGraph do
-  it 'has a version number' do
-    expect(EasyGraph::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
