b35ad2fe099c5919e59002e5543120637731b783
Julian Rogmans <rogmansj@gmail.com>
2016-05-17 00:55:08 -0700

    add sqlite3 as explicit dev dependency

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require "easy_graph"
+require "sqlite3"
+require "graphql"
 require "active_record"
 require "pry-byebug"
 require "require_all"
