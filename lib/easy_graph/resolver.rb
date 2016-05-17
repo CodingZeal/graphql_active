ecf185c8abf986fb4dc37a8ab8b79f6b64f3de78
Julian Rogmans <rogmansj@gmail.com>
2016-05-17 11:42:06 -0700

    Update resolver to take `find`, `all`, `update` and `create`
    - in order to have a more modular resolver class, separate out the
    functionality for updating, creating, finding one, and finding all the
    models in the database

--- lib/easy_graph/resolver.rb
+++ lib/easy_graph/resolver.rb
@@ -1,9 +1,20 @@
 module EasyGraph
   class Resolver
-    def self.resolve(model)
+    def self.all(model)
+      -> (_, _, _) { model.all }
+    end
+
+    def self.find(model)
+      -> (_, arguments, _) { model.find(arguments["id"]) }
+    end
+
+    def self.create(model)
+      -> (_, arguments, _) { model.create(arguments.to_h) }
+    end
+
+    def self.update(model)
       lambda do |_object, arguments, _context|
-        return model.find(arguments["id"]) if arguments["id"]
-        model.all
+        model.find(arguments.delete("id")).update(arguments.to_h)
       end
     end
   end
--- spec/easy_graph/resolver_spec.rb
+++ spec/easy_graph/resolver_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 RSpec.describe EasyGraph::Resolver do
   let(:model) { User }
 
-  let!(:users) do
+  before do
     model.create(
       [
         {
@@ -20,13 +20,30 @@ RSpec.describe EasyGraph::Resolver do
     )
   end
 
-  it "returns single item if id given" do
+  it "returns single item when calling #{described_class}.find" do
     params = [nil, { "id" => 1 }, nil]
-    expect(described_class.resolve(model).call(*params)).to eq users.first
+    expect(described_class.find(model).call(*params)).to eq model.first
   end
 
-  it "retruns a colection if no id is given" do
+  it "retruns a colection when calling #{described_class}.all" do
     params = [nil, {}, nil]
-    expect(described_class.resolve(model).call(*params)).to eq users
+    expect(described_class.all(model).call(*params)).to eq model.all
+  end
+
+  it "changes an item when calling #{described_class}.update" do
+    params = [
+      nil, { "id" => 1, "first_name" => "My new name", "age" => 1000 }, nil
+    ]
+    described_class.update(model).call(*params)
+    expect(model.first.first_name).to eq "My new name"
+    expect(model.first.age).to eq 1000
+  end
+
+  it "changes an item when calling #{described_class}.create" do
+    params = [
+      nil, { "first_name" => "EXAMPLE", "age" => 1000 }, nil
+    ]
+    described_class.create(model).call(*params)
+    expect(model.last.first_name).to eq "EXAMPLE"
   end
 end
