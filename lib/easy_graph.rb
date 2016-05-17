7f730ccd1f7256a06a4580e4050210ee9fc02c44
Julian Rogmans <rogmansj@gmail.com>
2016-05-17 10:48:11 -0700

    Build out shell mutation structure with support for update and delete

--- lib/easy_graph.rb
+++ lib/easy_graph.rb
@@ -7,6 +7,7 @@ require "easy_graph/relation_builder"
 require "easy_graph/attribute_builder"
 require "easy_graph/schema"
 require "easy_graph/query"
+require "easy_graph/mutation"
 require "easy_graph/type"
 require "easy_graph/resolver"
 
