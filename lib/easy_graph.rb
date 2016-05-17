f60fc2f4955fafa5a03f7a9df231925d9ac99958
Julian Rogmans <rogmansj@gmail.com>
2016-05-17 00:43:48 -0700

    change typo

--- lib/easy_graph.rb
+++ lib/easy_graph.rb
@@ -2,7 +2,7 @@ require "active_record"
 require "inflections"
 
 require "easy_graph/version"
-require "easy_graph/relation"
+require "easy_graph/relation_builder"
 
 module EasyGraph
   # Your code goes here...
