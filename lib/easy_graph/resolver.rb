239438f961c43a53afaa64562e778343485e95d6
Julian Rogmans <rogmansj@gmail.com>
2016-05-20 12:31:52 -0700

    Handle updating record better
    - allow for nil arguments to be passed in if you want to set something
    to nil
    - update and return the model accurately

--- lib/easy_graph/resolver.rb
+++ lib/easy_graph/resolver.rb
@@ -14,8 +14,13 @@ module EasyGraph
 
     def self.update(model)
       lambda do |_object, arguments, _context|
-        model.find(arguments.delete("id")).update(arguments.to_h)
+        model.update(arguments["id"], new_attributes(model, arguments.to_h))
       end
+    end
+
+    def self.new_attributes(model, arguments)
+      model_attributes = model.find(arguments["id"]).attributes.except("id")
+      model_attributes.merge(arguments.except("id"))
     end
   end
 end
--- spec/easy_graph/resolver_spec.rb
+++ spec/easy_graph/resolver_spec.rb
@@ -4,46 +4,73 @@ RSpec.describe EasyGraph::Resolver do
   let(:model) { User }
 
   before do
-    model.create(
-      [
-        {
-          first_name: "Foo",
-          last_name: "Bar",
-          age: 20
-        },
-        {
-          first_name: "Fiz",
-          last_name: "Buz",
-          age: 55
-        }
+    FactoryGirl.create_list(:user, 2)
+  end
+
+  context ".find" do
+    it "returns the model instance that matches the id argument passed" do
+      params = [nil, { "id" => 1 }, nil]
+      expect(described_class.find(model).call(*params)).to eq model.first
+    end
+  end
+
+  context ".all" do
+    it "returns the entire collection of the model" do
+      params = [nil, {}, nil]
+      expect(described_class.all(model).call(*params)).to eq model.all
+    end
+  end
+
+  context ".update" do
+    let(:first_name) { "EXAMPLE" }
+    let(:age) { 1000 }
+
+    before do
+      params = [
+        nil, { "id" => 1, "first_name" => first_name, "age" => age }, nil
       ]
-    )
+      described_class.update(model).call(*params)
+    end
+
+    it "changes the new values that were passed in" do
+      expect(model.first.first_name).to eq "EXAMPLE"
+      expect(model.first.age).to eq 1000
+    end
+
+    it "dosen't overwrite arguments with nil" do
+      expect(model.first.last_name).to eq "last_name_1"
+    end
+
+    context "when setting a value to nil" do
+      let(:age) { nil }
+
+      it "sets the appropiate value to nil" do
+        expect(model.first.age).to be_nil
+      end
+    end
   end
 
-  it "returns single item when calling #{described_class}.find" do
-    params = [nil, { "id" => 1 }, nil]
-    expect(described_class.find(model).call(*params)).to eq model.first
+  context ".create" do
+    it "creates a new model with the arguments passed in" do
+      params = [
+        nil, { "first_name" => "EXAMPLE", "age" => 1000 }, nil
+      ]
+      described_class.create(model).call(*params)
+      expect(model.last.first_name).to eq "EXAMPLE"
+    end
   end
 
-  it "retruns a colection when calling #{described_class}.all" do
-    params = [nil, {}, nil]
-    expect(described_class.all(model).call(*params)).to eq model.all
-  end
-
-  it "changes an item when calling #{described_class}.update" do
-    params = [
-      nil, { "id" => 1, "first_name" => "My new name", "age" => 1000 }, nil
-    ]
-    described_class.update(model).call(*params)
-    expect(model.first.first_name).to eq "My new name"
-    expect(model.first.age).to eq 1000
-  end
-
-  it "changes an item when calling #{described_class}.create" do
-    params = [
-      nil, { "first_name" => "EXAMPLE", "age" => 1000 }, nil
-    ]
-    described_class.create(model).call(*params)
-    expect(model.last.first_name).to eq "EXAMPLE"
+  it "builds attributes for updating model" do
+    attributes = {
+      "id" => 1,
+      "first_name" => "FOO",
+      "age" => 20
+    }
+    expected = {
+      "first_name" => "FOO",
+      "last_name" => "last_name_1",
+      "age" => 20
+    }
+    expect(described_class.new_attributes(model, attributes)).to eq expected
   end
 end
